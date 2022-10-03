4b1071a48f4edc0fa7a3fe166d08a3936cbafe6b
nurilloh2004 <nurillohpython@gmail.com>
2022-10-03 09:29:41 +0500

    *************

--- config/web/models.py
+++ config/web/models.py
@@ -7,4 +7,6 @@ from django.db import models
 class Products(models.Model):
     name = models.CharField(max_length=65)
     describtion = models.CharField(max_length=100)
-    
+
+    def __str__(self) -> str:
+        return self.name
--- config/web/views.py
+++ config/web/views.py
@@ -6,8 +6,7 @@ from django.views.generic import TemplateView
 from .models import *
 # Create your views here.
 
-class HomePage(TemplateView):
-    template_name = 'index.html'
+
 
 def products(request):
     all = Products.objects.all()
